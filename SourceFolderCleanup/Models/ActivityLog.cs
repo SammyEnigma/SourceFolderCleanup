7ff3ac70308255869ec198c8f1a2e44e06e583f3
Adam ONeil <adamosoftware@gmail.com>
2020-02-25 10:37:36 -0500

    fleshing out model classes

---
+++ SourceFolderCleanup/Models/ActivityLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SourceFolderCleanup.Models
+{
+    public class ActivityLog
+    {
+        public DateTime Timestamp { get; set; }
+        public Settings Settings { get; set; }
+        public List<Folder> DeletedFolders { get; set; }
+        public List<Folder> ArchivedFolders { get; set; }
+
+        public class Folder
+        {
+            public string Path { get; set; }
+            public long TotalSize { get; set; }
+        }
+    }
+}
--- SourceFolderCleanup/Models/Settings.cs
+++ SourceFolderCleanup/Models/Settings.cs
@@ -3,5 +3,10 @@
     public class Settings
     {
         public string[] SourcePaths { get; set; }
+        public bool DeleteBinAndObj { get; set; }
+        public bool DeletePackages { get; set; }
+        public int DeleteMonthsOld { get; set; }
+        public int ArchiveMonthsOld { get; set; }
+        public string ArchiveLocation { get; set; }
     }
 }
