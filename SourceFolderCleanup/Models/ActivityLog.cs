4378727290d614c7d816f7adad45fe0959544b59
Adam ONeil <adamosoftware@gmail.com>
2020-02-25 10:41:25 -0500

    added Archive class

--- SourceFolderCleanup/Models/ActivityLog.cs
+++ SourceFolderCleanup/Models/ActivityLog.cs
@@ -5,15 +5,21 @@ namespace SourceFolderCleanup.Models
 {
     public class ActivityLog
     {
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
         public Settings Settings { get; set; }
-        public List<Folder> DeletedFolders { get; set; }
-        public List<Folder> ArchivedFolders { get; set; }
+        public List<Folder> Deleted { get; set; }
+        public List<Archive> Archived { get; set; }
 
         public class Folder
         {
             public string Path { get; set; }
             public long TotalSize { get; set; }
+        }
+
+        public class Archive
+        {
+            public string SourcePath { get; set; }
+            public string OutputZipFile { get; set; }
         }
     }
 }
